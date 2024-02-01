9ba8cb37bc76f58f1ffa5782a910fb5f8377900e
AryaEm <ary4maulanaa@gmail.com>
2024-02-01 08:41:37 +0700

    first commit

---
+++ main.java
@@ -0,0 +1,12 @@
+package enkapsulasi;
+
+class main extends person{
+    public static void main(String[] args) {
+
+    person mPerson = new person();
+    mPerson.setName("Alok");
+
+    System.out.println("Person Name: " + mPerson.getName());
+
+    }
+}
